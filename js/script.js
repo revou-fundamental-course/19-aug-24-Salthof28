2b409025924390712d4ca8f673a161206957489d
unknown <salmanalthof31@gmail.com>
2024-08-29 00:12:00 +0700

    perbaikan responsive dan penambahan replacemant name di bagian awal

--- js/script.js
+++ js/script.js
@@ -1,3 +1,10 @@
+// untuk nama awal di article
+replaceName();
+function replaceName() {
+    let name = prompt("Masukan nama anda?", "");
+    document.getElementById('name').innerHTML = name;
+  }  
+
 // Program stcky navbar
 window.onscroll = function() {myFunction()};
 var navbar = document.getElementById('navbar');
@@ -43,43 +50,36 @@ setInterval(() => nextSlide(1), 3000);
 // Program add massageUS
 
 let username = document.querySelector('#input-name')
-
 let date = document.querySelector('#input-Date')
-
 var gender = document.getElementsByName('input_gender');
-
 let pesan = document.querySelector('#input-Pesan')
-
 const submitBtn = document.querySelector('.Button_style')
 const commentsCont = document.querySelector('.comment_container')
 
 submitBtn.addEventListener('click', submitFeedback);
-
+//untuk variabel array item newfeedback
 feedbackArr = [];
 
 function submitFeedback(e) {  
-    
+    //mengambil value nama dan tanggal lahir
     const namevalue = username.value;
     const datevalue = date.value;
-
+    //untuk waktu realtime saat massage submit
     var dateString = new Date().toLocaleString("en-US", {timeZone: "Asia/Jakarta"});
     var formattedString = dateString.replace(", ", " - ");
     const pesanvalue = pesan.value;
-
+    //untuk mengambil value jenis kelamin
     for (i = 0; i < gender.length; i++) {
         if (gender[i].checked){
             gendervalue = gender[i].value;
         }
     }
-
+    //untuk validasi submit dan proses penambahan comment di bagian massage
     if (namevalue =='' || datevalue == '' || gendervalue == '' || pesanvalue == '') {
         alert('Maaf Proses input tidak dapat diproses karena ada inputan yang kosong');
     }
     else {
         alert ('Proses Input Data Behasil');
-        document.getElementById('name').innerHTML = namevalue;
-
-
 
         newFeedback = {
             "id": Math.floor((Math.random() * 1000)+ 1),
@@ -89,10 +89,9 @@ function submitFeedback(e) {
             "clock": formattedString,
             "pesans":pesanvalue
         }
-
+        //pengambilan data array
         feedbackArr.push(newFeedback);
-
-        
+        //menjalankan function addFeedback untuk menambahkan komen di massage
         addFeedback(newFeedback);
 
     }
@@ -101,9 +100,14 @@ function submitFeedback(e) {
 }
 
 function addFeedback(item){
+    //menentukan huruf awal dari nama yang di tulis
     const letter = (item.names).charAt(0);
+    //membuat div di bagian html
     const div = document.createElement('div');
+    //dari div yang telah ditambahkan di tambah class: comment_card
     div.classList = 'comment_card';
+    //menulis tab massage ke index.html saat submit, dan mengambil item array dari feedbackArr
+    //nilai array yang diambil untuk ditampilkan pada massage saat submit
     div.id = item.id;
     div.innerHTML = `
     <div class = "pic center_display">
@@ -127,7 +131,7 @@ function addFeedback(item){
                         </p>
                     </div>
     `;
-    // memasukan feedback ke list "div class = "comment_container""
+    // memasukan "div class = comment_card", dan "div.innerHTML" ke dalam "div class = "comment_container"" yang ada di html
     commentsCont.insertAdjacentElement('beforeend', div);
 
 }
